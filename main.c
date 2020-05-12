d244ccacad7feb8fc8fd44c4a29d8f9fbaf10662
Guilherme <guilhermeddc@gmail.com>
2020-05-11 22:05:28 -0300

    criado os tres primeiros casos

--- main.c
+++ main.c
@@ -1,128 +1,161 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-typedef struct cliente
-{
-  struct c *anterior;
+typedef struct cliente {
+  int id;
   char nome[50];
-  char rg[8];
+  char rg[10];
   struct c *prox;
 } c;
 
-//excluir a pessoa depois de atend�-la
-void excluir(c **list)
-{
-	char nm[50];
-	c *anterior, *atual;
-	
-	printf("\nInforme o nome do cliente que deseja excluir: ");
-	gets(nm);
-	
-	anterior = *list;
-	atual = *list;
-	
-	while(atual != NULL){
-		if(atual->nome[50] == nm[50]){
-			free(atual);
-			anterior->prox = atual->prox;
-		}
-	}
+c* alocarMemoria () {
+  c *cliente = (cliente*)malloc(sizeof(cliente));
+  return cliente;
 }
 
+void inserirFim (c **lista, int id, char nome[50], char rg[10]) {
+  c *novo = alocarMemoria();
 
-//inserir nova pessoa na lista de espera
-void insere_final(c **lista)
-{
- 	c *n, *aux;
-  	n = (c*)malloc(sizeof(c));
-  
-  	fflush(stdin);
-  	printf("\nDigite o nome do cliente: ");
-  	gets(n->nome);
-  	fflush(stdin);
-  	printf("\nDigite o rg do cliente: ");
-  	gets(n->rg);
-  
-  	n->prox = NULL;
-  	
-  	if(*lista == NULL){
-    	*lista = n;
-	}
-	else{
-		aux = *lista;
-		while (aux->prox != NULL)
-      	aux = aux->prox;
-    	aux->prox = n;	
+  novo->id = id;
+  strcpy(novo->nome, nome);
+  strcpy(novo->rg, rg);
+
+  if (*lista == NULL) {
+    novo->prox = NULL;
+    lista = novo;
+  } else {
+    c* aux = lista;
+    while (aux->prox != NULL) {
+      aux = aux->prox;
+    }
+    novo->prox = NULL;
+    aux->prox = novo;
   }
 }
 
-//buscar seu rg para ver quantos falta 
-void lista_buscar(c **list)
-{
-	int valor;
-	
-	printf("\nInforme o numero do cliente desejado: ");
-	scanf("%d", &valor);
-	
-	c *n;
-	
-    for(n=(*list); n != NULL; n->prox){
-    	if(valor == n->rg){
-        	printf("\n %s  ->  %d.", n->nome, n->rg);
-        }
-    }
+void inserirInicio (c **lista, int id, char nome[50], char rg[10]) {
+  c *aux = alocarMemoria();
+
+  aux->id = id;
+  strcpy(aux->nome, nome);
+  strcpy(aux->rg, rg);
+
+  if (*lista == NULL) {
+    aux->prox = NULL;
+    lista = aux;
+  } else {
+    aux->prox = *lista;
+    *lista = aux;
+  }
 }
 
-int main()
-{
+void exibirProximos (c *lista) {
+  c *aux = lista;
+
+  if (aux != NULL) {
+    do {
+      printf("id - %d\n", aux->id);
+      printf("nome - %s\n", aux->nome);
+      printf("rg - %s\n\n", aux->rg);
+      aux = aux->prox;
+    } while (aux != NULL);
+  }
+}
+
+void chamarProximo (c **lista) {
+  c *aux = lista;
+  printf("id - %d\n", aux->id);
+  printf("nome - %s\n", aux->nome);
+  printf("rg - %s\n\n", aux->rg);
+  lista = aux->prox;
+}
+
+int main() {
   c *cliente = NULL;
+  c *clientePreferencial = NULL;
+  int identificador = 1;
   int op;
-  do
-  {
-    printf("Selecione uma op��o abaixo:\n");
+
+  do {
+    printf("Selecione uma opcao abaixo:\n");
     printf("1) Cadastrar-se na lista de espera;\n");
-    printf("2) Chamar o proximo aguardante;\n");
-    printf("3) Exibir os proximos aguardantes;\n");
+    printf("2) Chamar o proximo da fila;\n");
+    printf("3) Exibir os proximos da fila;\n");
     printf("4) Visualizar quantos tem antes;");
     printf("Tecle 0 e ENTER para sair\n");
-    printf("0 - sair\n");
     scanf("%d", &op);
 
-    switch (op)
-    {
-    case 0:
-    {
-      break;
-    }
-    case 1:
-    {
-      break;
-    }
-    case 2:
-    {
-      break;
-    }
-    case 3:
-    {
-      break;
-    }
-    case 4:
-    {
-      break;
-    }
-    case 5:
-    {
-      break;
-    }
-    case 6:
-    {
-      break;
-    }
-    default:
-    {
-      printf("Digito incorreto\n\n");
-      break;
-    }
+    switch (op) {
+      case 1: {
+        identificador++;
+        int id = identificador;
+        char nome[50];
+        char rg[10];
+        int preferencial = 0;
+
+        printf("Cadastro:\n");
+        fflush(stdin);
+        printf("Digite seu nome:\n");
+        gets(nome);
+        fflush(stdin);
+        printf("Digite seu rg:\n");
+        gets(rg);
+        printf("Cliente preferencial? (1) sim | (2) nao\n");
+        scanf("%d", &preferencial);
+
+        do {
+          switch (preferencial) {
+            case 1: {
+              inserirFim(&clientePreferencial, id, nome, rg);
+              break;
+            }
+            case 1: {
+              inserirFim(&cliente, id, nome, rg);
+              break;
+            }
+            default: {
+              printf("Digito incorreto, tente novamente.\n (1) sim | (2) nao\n")
+              break;
+            }
+          }
+        } while (preferencial != 1 || preferencial != 2)
+
+        break;
+      }
+      case 2: {
+        if (clientePreferencial != NULL) {
+          chamarProximo(&clientePreferencial);
+        } else if (cliente != NULL) {
+          chamarProximo(&cliente)
+        } else {
+          printf("\nNao ha mais clientes para serem atendidos.");
+        }
+        break;
+      }
+      case 3: {
+        if (clientePreferencial != NULL) {
+          exibirProximos(clientePreferencial);
+        } else if (cliente != NULL) {
+          exibirProximos(cliente)
+        } else {
+          printf("\nNao ha mais clientes para serem atendidos.");
+        }
+        break;
+      }
+      case 4: {
+        break;
+      }
+      case 5: {
+        break;
+      }
+      case 6: {
+        break;
+      }
+      default: {
+        printf("Digito incorreto\n\n");
+        break;
+      }
     }
   } while (op != 0);
   return 0;
